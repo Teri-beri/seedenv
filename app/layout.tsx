4e8d647ed083b89de893434e3f01a6bd264e9075
Teri-beri <Teri-beri@users.noreply.github.com>
2026-09-09 22:48:26 -0400

    Add iOS PWA metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -57,7 +57,7 @@ export const metadata: Metadata = {
       { url: "/icons/icon-192.png", sizes: "192x192", type: "image/png" },
       { url: "/icons/icon-512.png", sizes: "512x512", type: "image/png" },
     ],
-    apple: [{ url: "/apple-touch-icon.png", sizes: "192x192", type: "image/png" }],
+    apple: [{ url: "/icons/apple-touch-icon.png", sizes: "192x192", type: "image/png" }],
   },
 };
 
